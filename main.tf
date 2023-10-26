8ef79cc90f023a875b0d83a3d8bcacc342687775
Aaron Ow <aaronowdotcom@gmail.com>
2023-10-26 14:52:56 +0800

    comment out cloud block

---
+++ main.tf
@@ -0,0 +1,37 @@
+terraform {
+
+  /*
+cloud {
+    organization = "alienCompany"
+    workspaces {
+      name = "learn-tfc-aws"
+    }
+  }
+*/
+
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+resource "aws_instance" "app_server" {
+  #  ami           = "ami-06018068a18569ff2"
+  ami           = var.aws_instance-app_server-ami
+  instance_type = "t2.micro"
+
+  tags = {
+    #    Name = "ExampleAppServerInstance"
+    Name = var.instance_name
+  }
+}
+
